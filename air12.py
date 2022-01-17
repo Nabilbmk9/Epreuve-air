7c7a1773a39b603c4fc11d4f954898048090da04
Nabilbmk9 <nabil.bmk9@gmail.com>
2022-01-17 17:25:05 +0100

    Le roi des tris

--- air12.py
+++ air12.py
@@ -1,25 +1,37 @@
-Liste = [7, 4, 2, 5, 1, 3, 8]
+import sys
 
-def tri_rapide(L):
-    pivot = L[(len(L)-1)//2]
+#Gestion erreur
+for x in sys.argv[1:]:
+    if not x.isnumeric():
+        print("erreur.")
 
-    for i in range(0, L[((len(L)-1)//2)]-1):       
-        for j in range(L[(len(L)-1)//2]-1, len(L)):
-            print(f"i : {i}")
-            print(f"j : {j}")
-        
-            if L[i] > pivot:
-                if L[j] < pivot:
-                    L[i], L[j] = L[j], L[i]
-            
-            if L[i] > pivot:
-                if L[j] < pivot:
-                    L[j], L[(len(L)-1)//2] = L[(len(L)-1)//2], L[j] 
-            
-            
-            
-    print(L)
-    
+ #Fonction       
+def trirapide(L=sys.argv[1:]):
+    """trirapide(L): tri rapide (quicksort) de la liste L"""
+    def trirap(L, g, d):
+        pivot = L[(g+d)//2]
+        i = g
+        j = d
+        while True:
+            while L[i]<pivot:
+                i+=1
+            while L[j]>pivot:
+                j-=1
+            if i>j:
+                break
+            if i<j:
+                L[i], L[j] = L[j], L[i]
+            i+=1
+            j-=1
+        if g<j:
+            trirap(L,g,j)
+        if i<d:
+            trirap(L,i,d)
+ 
+    g=0
+    d=len(L)-1
+    trirap(L,g,d)
+    for i in L:
+        print(i, end=" ")
 
-tri_rapide(Liste)
-
+trirapide()
