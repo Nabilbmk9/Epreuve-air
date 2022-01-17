7ae17a97c9a03e1edbc16df1eb6fb9307d45d864
Nabilbmk9 <nabil.bmk9@gmail.com>
2022-01-17 17:32:44 +0100

    Air : ok

---
+++ air14.py
@@ -0,0 +1,3 @@
+#Air : ok
+
+print("J’ai terminé l’Épreuve de l’Air et c’était assez : Ca va. J'ai grave galerer sur le tri rapide, j'avais des gros proleme de logique. Mais dans l'ensemble je me sent plutot a l'aise. Je me demande si mon code est bien structuré")
